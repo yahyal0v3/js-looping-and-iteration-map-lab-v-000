464275a143d134b8f60735bc0224951d0fac9ee2
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-01 03:31:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 function lowerCaseDrivers(collection) {
   return collection.map(element => element.toLowerCase())
 }
+
+function nameToAttributes(collection) {
+  return collection.map(element => 
+    arrayName = element.split(" ")
+    return {firstName: arrayName[0], lastName: arrayName[1]}
+  )
+}
